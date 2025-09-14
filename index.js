ca9532f9cd4a8ce54669d4d87f94916592c3b4cb
Hckr03 <gabu.1991@gmail.com>
2025-09-14 18:52:26 -0300

    mucho por aprender aún

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 
 var app = express();
 
@@ -11,8 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: 'No file uploaded' });
+  const { originalname, mimetype, size } = req.file = req.file;
+  res.json({ 
+    name: originalname,
+    type: mimetype,
+    size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
